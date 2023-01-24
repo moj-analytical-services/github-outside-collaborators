d4d5abea1df5e2fd128dfed21a88a511462346ce
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-01-24 14:51:21 +0000

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   # `backend` blocks do not support variables, so the following are hard-coded here:
   # - S3 bucket name, which is created [here](https://github.com/ministryofjustice/cloud-platform-environments/blob/main/namespaces/live.cloud-platform.service.justice.gov.uk/operations-engineering/resources/s3-ap-gh-collab-repo.tf)
-  backend "s3-ap-gh-collab-repo" {
+  backend "s3" {
     encrypt        = true
     bucket         = "cloud-platform-7432180341a66d72d042a5a59ee86291"
     region         = "eu-west-2"
