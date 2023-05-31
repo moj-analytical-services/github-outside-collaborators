303ef2d1024e0d6057043bf7643a54249b9ed160
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2023-05-31 15:18:17 +0100

    Add new CPS collaborators

--- terraform/cjs-scorecard.tf
+++ terraform/cjs-scorecard.tf
@@ -12,5 +12,25 @@ module "cjs-scorecard" {
       added_by     = "Nick Walters nick.walters@digital.justice.gov.uk"
       review_after = "2023-06-13"
     },
+    {
+      github_user  = "DaveAllanCPS"
+      permission   = "push"
+      name         = "Dave Allan"
+      email        = "david.allan@cps.gov.uk"
+      org          = "CPS"
+      reason       = "They provide us data, and we are sharing access to the code we use to process and validate their data to help with their quality assurance processes"
+      added_by     = "Katherine Breeze katharine.breeze@justice.gov.uk"
+      review_after = "2024-05-30"
+    },
+    {
+      github_user  = "SophieYoung11"
+      permission   = "push"
+      name         = "Sophie Young"
+      email        = "sophie.young@cps.gov.uk"
+      org          = "CPS"
+      reason       = "They provide us data, and we are sharing access to the code we use to process and validate their data to help with their quality assurance processes"
+      added_by     = "Katherine Breeze katharine.breeze@justice.gov.uk"
+      review_after = "2024-05-30"
+    },
   ]
 }
