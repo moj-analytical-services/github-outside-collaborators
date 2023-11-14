1728b381a89734559607557f9a699bbce93a383b
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-11-14 13:15:46 +0000

    Remove full org member from dsa_data_linking.tf (#105)

--- terraform/dsa_data_linking.tf
+++ terraform/dsa_data_linking.tf
@@ -3,16 +3,6 @@ module "dsa_data_linking" {
   repository = "dsa_data_linking"
   collaborators = [
     {
-      github_user  = "mjchalkley"
-      permission   = "pull"
-      name         = "tbc"
-      email        = "tbc"
-      org          = "tbc"
-      reason       = "tbc"
-      added_by     = "Nick Walters nick.walters@digital.justice.gov.uk"
-      review_after = "2023-12-10"
-    },
-    {
       github_user  = "didartls"
       permission   = "pull"
       name         = "Didar Hussain"
