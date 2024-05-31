6124ab210071a1752e2eacddb7dd58c0a6777cbf
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2024-05-31 11:48:20 +0100

    Corrected approval email

--- terraform/airflow-legislation-documentstore.tf
+++ terraform/airflow-legislation-documentstore.tf
@@ -9,7 +9,7 @@ module "airflow-legislation-documentstore" {
       email        = "liam.wilkinson@cabinetoffice.gov.uk"
       org          = "Cabinet Office/No 10"
       reason       = "Collaboration with no10 on a x-govt project relating to legislation."
-      added_by     = "sam.pepper@digital.justice.gov.uk"
+      added_by     = "chloe.pugh1@justice.gov.uk"
       review_after = "2025-05-29"
     },
   ]
